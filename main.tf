952d20365666b2ab4c688c518e3e29e4203a94bc
Ismar <ismarprobst@gmail.com>
2024-01-06 23:02:17 -0300

    Criacao de ansible-playbook

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
   key_name = "iac-alura"
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "Terraform Ansible Python"
   }
 }
 
